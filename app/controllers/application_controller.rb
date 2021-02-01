b94686d0e803476fb59500d9a1727f777a1fa66c
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-02-01 12:02:35 -0500

    commit tree 451bec60f9466638c3b9377caffff06120fa46c7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,42 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to '/articles'
+  end
+
+  get '/articles' do 
+    @articles = Article.all
+    erb :index
+  end 
+
+  get '/articles/new' do 
+    @article = Article.new 
+    erb :new
+  end 
+
+  post '/articles' do 
+    @article = Article.create(params)
+    redirect to '/articles/#{@article.id}'
+  end 
+
+  get '/articles/:id' do 
+    @article = Article.find_by_id(params[:id])
+    erb :show
+  end 
+
+  get '/articles/:id/edit' do 
+    @article = Article.find_by_id(params[:id])
+    erb :edit 
+  end 
+
+  patch "/articles/:id" do
+    @article = Article.find_by_id(params[:id])
+    @article.update(params[:article])
+    redirect to "/articles/#{@article.id}"
+  end
+
+  delete "/articles/:id" do
+    Article.destroy(params[:id])
+    redirect to "/articles"
   end
 end
